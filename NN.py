c8a55abab808f5d7a7fa892969c82743289fae7d
prupireddy <prupireddy@gmail.com>
2020-03-31 18:26:41 -0500

    Added Buggy Image Import Code

--- NN.py
+++ NN.py
@@ -1,5 +1,9 @@
 import torch
+import os
 import numpy as np
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+
+root = os.getcwd()
+dataset = datasets.ImageFolder(root,transforms.ToTensor())
