17778c18e061f4d97a2eb7229b49b1cf2793d071
prupireddy <prupireddy@gmail.com>
2020-04-01 17:17:26 -0500

    In progress

--- NN.py
+++ NN.py
@@ -1,3 +1,4 @@
+import tifffile
 import torch
 import os
 import numpy as np
@@ -6,12 +7,14 @@ from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader 
 
+def my_tiff_loader(filename):
+    return tifffile.imread(filename)
 
 patientNumber = 10
 patientNumber = str(patientNumber)
-root = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
-os.chdir(root)
-data = datasets.ImageFolder(root,transforms.ToTensor())
+storage = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
+os.chdir(storage)
+data = datasets.ImageFolder(root = storage,loader = my_tiff_loader ,transform = transforms.ToTensor())
 Data = DataLoader(data, batch_size = 2, shuffle = True)
 for idx, (x,y) in enumerate(Data):
     print(idx)
