ef8fd9dc50bd7fd4c9b4e7c0ba6d3f32d4eb1fda
prupireddy <prupireddy@gmail.com>
2020-04-01 11:13:57 -0500

    Added code to change working directory

--- NN.py
+++ NN.py
@@ -5,5 +5,8 @@ from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
-root = os.getcwd()
+patientNumber = 10
+patientNumber = str(patientNumber)
+root = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
+os.chdir(root)
 dataset = datasets.ImageFolder(root,transforms.ToTensor())
