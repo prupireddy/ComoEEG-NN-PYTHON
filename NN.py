575d24737818c1f0f22a82c295b86d9457fa6280
prupireddy <prupireddy@gmail.com>
2020-04-01 15:34:13 -0500

    trying to figure out what is going on

--- NN.py
+++ NN.py
@@ -4,9 +4,14 @@ import numpy as np
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader 
+
 
 patientNumber = 10
 patientNumber = str(patientNumber)
 root = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
 os.chdir(root)
-dataset = datasets.ImageFolder(root,transforms.ToTensor())
+data = datasets.ImageFolder(root,transforms.ToTensor())
+Data = DataLoader(data, batch_size = 2, shuffle = True)
+for idx, (x,y) in enumerate(Data):
+    print(idx)
