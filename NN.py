abd17a973ebd9fca8a785bc09e623aeb108852cc
prupireddy <prupireddy@gmail.com>
2020-04-02 09:47:43 -0500

    With redundant loaders

--- NN.py
+++ NN.py
@@ -22,13 +22,11 @@ def read_tiff(path):
     path - Path to the multipage-tiff file
     """
     img = Image.open(path)
-    # img.seek(0)
-    # tiffarray = np.zeros((img.height,img.width,22))
-    tiffarray = []
+    img.seek(0)
+    tiffarray = np.zeros((22,img.height,img.width))
     for i in range(img.n_frames):
         img.seek(i)
-        # tiffarray[:,:,1] = np.array(img)
-        tiffarray.append(np.array(img))
+        tiffarray[i,:,:] = np.array(img)
     return np.array(tiffarray)
 
 def my_tiff_loader(filename):
@@ -38,7 +36,7 @@ patientNumber = 10
 patientNumber = str(patientNumber)
 storage = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
 storage = storage + "\ictal\P10_1.TIFF"
-data = read_tiff(storage)
+data = tiff(storage)
 # os.chdir(storage)
 # data = datasets.ImageFolder(root = storage,loader = read_tiff ,transform = transforms.ToTensor())
 # Data = DataLoader(data, batch_size = 2, shuffle = True)
