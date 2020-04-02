e286fcb018a551225cdcb84d297af71da9d58caa
prupireddy <prupireddy@gmail.com>
2020-04-02 12:18:23 -0500

    Functioning loader - just no seperation yet into train and testing sets

--- NN.py
+++ NN.py
@@ -1,44 +1,30 @@
 import tifffile
-import imagecodecs
 import torch
-from PIL import Image
 import os
 import numpy as np
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader 
-from PIL import Image
-import numpy as np
-from skimage import io
 import matplotlib.pyplot as plt
 
-def tiff(path):
-    return io.imread(path)
-
-
-def read_tiff(path):
-    """
-    path - Path to the multipage-tiff file
-    """
-    img = Image.open(path)
-    img.seek(0)
-    tiffarray = np.zeros((22,img.height,img.width))
-    for i in range(img.n_frames):
-        img.seek(i)
-        tiffarray[i,:,:] = np.array(img)
-    return np.array(tiffarray)
-
 def my_tiff_loader(filename):
-    return tifffile.imread(filename)
-
+    original = tifffile.imread(filename)
+    C,H,W = original.shape
+    final = np.zeros((H,W,C))
+    for i in range(C):
+        final[:,:,i] = original[i,:,:]
+    return final
+    
 patientNumber = 10
 patientNumber = str(patientNumber)
-storage = "D:\ComoEEG\Tyler Data\Patient " + patientNumber
-storage = storage + "\ictal\P10_1.TIFF"
-data = tiff(storage)
+storage = "D:\ComoEEG\Tyler Data\Patient " + patientNumber 
+# storage = "D:\ComoEEG\Tyler Data\Patient 10\ictal"
 # os.chdir(storage)
-# data = datasets.ImageFolder(root = storage,loader = read_tiff ,transform = transforms.ToTensor())
-# Data = DataLoader(data, batch_size = 2, shuffle = True)
-# for idx, (x,y) in enumerate(Data):
-#     print(idx)
+# storage = storage + "\P10_1.TIFF"
+# data = my_tiff_loader(storage)
+trans = transforms.ToTensor()
+Data = datasets.ImageFolder(root = storage, loader = my_tiff_loader,transform = trans)
+DataSet = DataLoader(Data, batch_size = 2, shuffle = False)
+for idx, (x,y) in enumerate(Data):
+    print(x.shape)
